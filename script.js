83484532c787f2daf6e6d133e60e2d3327d1d6d2
Nikows <44471@aeaag.pt>
2026-05-25 21:26:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function submitGuess() {
           document.getElementById("message").textContent = "Do not insert letters.";
 	  } else if (guess === "") {
 		  document.getElementById("message").textContent = "Do not enter empty space.";
-      } else if (guess === /[^a-zA-Z0-9]/.test(guess)) {
+      } else if (/[^a-zA-Z0-9]/.test(guess)) {
 		  document.getElementById("message").textContent = "Do not enter special characters.";
 	  } else {
           document.getElementById("message").textContent = "Enter exactly 5 numbers.";
